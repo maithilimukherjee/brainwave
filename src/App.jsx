42efc2e33fdd689005b20e7a22644fddc38fc434
Maithili Mukherjee <86041684+maithilimukherjee@users.noreply.github.com>
2025-07-10 15:24:50 +0530

    updated header and added constants

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './index.css';
+import ButtonGradient from './assets/svg/ButtonGradient';
+import Button from './components/Button';
+import Header from './components/Header';
 
-function App() {
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <>
+    <div className='pt-[4.75rem] lg: pt-[5.25 rem] overflow-hidden'>
+      <Header />
+    </div>
+    <ButtonGradient />
+    </>
+  );
+};
 
 export default App;
 
