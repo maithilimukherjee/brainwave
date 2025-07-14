2ede1e22818db23bcfc4e817c085f2c02858eb21
Maithili Mukherjee <86041684+maithilimukherjee@users.noreply.github.com>
2025-07-14 12:24:36 +0530

    final commit omg

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import Benefits from './components/Benefits';
 import Collaboration from './components/Collaboration';
 import Services from './components/Services';
 import Pricing from './components/Pricing';
+import Roadmap from './components/Roadmap'
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -20,6 +22,8 @@ const App = () => {
       <Collaboration />
       <Services />
       <Pricing />
+      <Roadmap />
+      <Footer />
     </div>
     <ButtonGradient />
     </>
--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,34 +1,55 @@
-import { smallSphere, stars } from "../assets"
-import Section from "./Section"
+import Section from "./Section";
+import { smallSphere, stars } from "../assets";
+import Heading from "./Heading";
+import PricingList from "./PricingList";
+import { LeftLine, RightLine } from "./design/Pricing";
 
 const Pricing = () => {
   return (
     <Section className="overflow-hidden" id="pricing">
-      <div className="container relative z-10">
-        
-        <div className="relative hidden lg:flex items-center justify-center mb-[6.5rem] animate-[float_3s_ease-in-out_infinite]">
-          
-          
-          <img
-            src={stars}
-            alt="stars"
-            className="absolute top-1/2 left-1/2 w-[950px] max-w-none -translate-x-1/2 -translate-y-1/2 z-0"
-            width={950}
-            height={400}
-          />
-
+      <div className="container relative z-2">
+        <div className="hidden relative justify-center mb-[6.5rem] lg:flex animate-float">
           <img
             src={smallSphere}
-            alt="sphere"
-            className="relative z-10 w-[255px] h-[255px]"
+            className="relative z-1"
             width={255}
             height={255}
+            alt="Sphere"
           />
+          <div className="absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none">
+            <img
+              src={stars}
+              className="w-full"
+              width={950}
+              height={400}
+              alt="Stars"
+            />
+          </div>
         </div>
 
+        <Heading
+          tag="Get started with Brainwave"
+          title="Pay once, use forever"
+          className='md:text-center lg:text-center'
+        />
+
+        <div className="relative">
+          <PricingList />
+          <LeftLine />
+          <RightLine />
+        </div>
+
+        <div className="flex justify-center mt-10">
+          <a
+            className="text-xs font-code font-bold tracking-wider uppercase border-b"
+            href="/pricing"
+          >
+            See the full details
+          </a>
+        </div>
       </div>
     </Section>
-  )
-}
+  );
+};
 
-export default Pricing
+export default Pricing;
